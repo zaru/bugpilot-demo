bbdf4b2dbbab72aef6434c5a6efeedb744f175d7
zaru <zarutofu@gmail.com>
2024-02-20 19:32:01 +0900

    bugpilot setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
+import { default as bugpilotConfig } from "./bugpilot.config.js";
+import { withBugpilot } from "@bugpilot/plugin-nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default nextConfig;
+export default withBugpilot(nextConfig, bugpilotConfig);
